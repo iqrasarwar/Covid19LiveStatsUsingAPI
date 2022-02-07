f20d4ed30864cd9e390ca7a7f716d756c68618be
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-07 19:19:33 +0500

    Update style.php

--- covid19/CSS/style.php
+++ covid19/CSS/style.php
@@ -1,1 +1,35 @@
 
+.nav-link, .navbar-brand
+{
+    color:red !important;
+    font-weight: bold;
+    font-size: larger;
+}
+#virusImg
+{
+    width: 40px !important;
+    height: 40px !important;
+    animation: rotation 2s infinite linear;
+}
+#humman
+{
+    width: 300px;
+    height: 300px;
+    animation: stretch 1.5s ease-out 0s alternate infinite none running;
+}
+@keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+  @keyframes stretch {
+    0% {
+      transform: scale(.3);
+    }
+    100% {
+      transform: scale(1.5);
+    }
+  }
