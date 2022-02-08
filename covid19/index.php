616334b7ef12baeb1db9e974c90b8ea893b1a462
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-08 23:12:36 +0500

    Add files via upload

---
+++ covid19/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'links.php';?>
+    <?php include 'css/style.php';?>
+    <title>Covid19</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand pl-5" href="#">Covid19 Live Updates</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse pr-5" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Live Stats<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Symptoms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PreCautions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="row w-100 h-100 ">
+    <div class="col-lg-5 col-md-5 col-12 dd d-flex justify-content-center align-items-center">
+      <img src="media/h.png" alt="hummans Together" id="humman">
+    </div>
+    <div class="d-flex col-lg-7 dd col-md-7 col-12  justify-content-center align-items-center text-center">
+      <h3>C<span><img src="media/virus.png" alt="O" id="virusImg"></span>VID-19 Situation!</h3>
+      <p>The coronavirus, or COVID-19, is inciting panic for a number of reasons. It's a new virus, meaning no one has immunity.
+      It is highly contagious, meaning it spreads fast. Its novelty means that scientists aren't completely sure as to how
+      it behaves since they have very little history to go on.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 text-center redHead">
+      <h2>Covid-19 Update</h2>
+    </div>
+    <div class="col-12 d-flex justify-content-center align-items-center text-center">
+      <div class="col-3">
+        <h3 class="count">
+        24269927352
+        </h3>
+        <p>
+        Active Cases in World
+        </p>
+      </div>
+      <div class="col-3">
+      <h3 class="count">
+      934602467
+      </h3>
+      <p>
+      Active cases in Pakistan
+      </p>
+      </div>
+      <div class="col-3">
+      <h3 class="count">
+      32075273
+      </h3>
+      <p>
+      Total deaths in world
+      </p>
+      </div>
+      <div class="col-3">
+      <h3 class="count">
+      293659235
+      </h3>
+      <p>
+      Recovered Cases
+      </p>
+      </div>
+    </div>
+  </div>
+  <div id="about">
+    <div class="col-12 text-center redHead">
+      <h2>
+      About Covid-19</h2>
+    </div>
+    <div class="desc d-flex justify-content-center align-items-center">
+      <div class="col-5 imgg">
+        <img src="media/covid.jfif" alt="covid image">
+      </div>
+      <div class="col-5">
+        <p>
+        COVID-19 is a virus that can cause mild to severe respiratory illness, including death. The best preventive measures include getting vaccinated, wearing a mask, staying 6 feet apart, washing hands often and avoiding sick people.
+        Coronaviruses are a family of viruses that can cause respiratory illness in humans. They are called “corona” because of crown-like spikes on the surface of the virus. Severe acute respiratory syndrome (SARS), Middle East respiratory syndrome (MERS) and the common cold are examples of coronaviruses that cause illness in humans.
+        </p>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
