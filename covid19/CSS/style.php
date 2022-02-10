85965dbdbbb374ca9ae3af7a924847a706bca144
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-02-10 23:07:12 +0500

    Add files via upload

--- covid19/CSS/style.php
+++ covid19/CSS/style.php
@@ -1,4 +1,8 @@
 <style type="text/css">
+body{
+  padding:0 !important;
+  font-family: 'Chakra Petch', sans-serif;
+}
 .nav-link, .navbar-brand
 {
     color:red !important;
@@ -42,5 +46,48 @@
  .imgg img{
    width:90%;
    height:50%;
+ }
+ .symptoms img{
+   width:130px;
+   height:130px;
+   border-radius:65px;
+   border:1px inset red;
+ }
+ .symptoms p{
+font-weight:bold;
+ }
+
+ .prevent img{
+   width:600px;
+   height:300px;
+   border:1px solid red;
+   border-radius:5px;
+ }
+ .preventImg img{
+  width:50px;
+   height:50px;
+   border-radius:65px;
+   border:1px inset red;
+ }
+ .sub-section{
+   background-color:#fff7f7 !important;
+ }
+ .red{
+   color:red;
+   display:inline;
+   font-weight:bold;
+ }
+ .row{
+   margin:0px !important;
+ }
+ h2{
+   margin-top:20px;
+   margin-bottom:15px;
+ }
+ .outer div{
+padding-bottom:10px;
+ }
+ .last{
+   padding:0px !important;
  }
 </style>
